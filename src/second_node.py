793629efe643b497308363eeefc020237a4f0fc4
Abdalla-El-gohary <rabyalalla@gmail.com>
2025-02-24 21:01:50 +0200

    update second node

--- src/second_node.py
+++ src/second_node.py
@@ -18,7 +18,7 @@ my_baudrate = 115200
 kinematic = kinematicModel(wheel_radius, lx, ly)
 robot = RobotController(port=my_port, baudrate=my_baudrate, kinematics=kinematic)
 
-# features flags
+# Features flags
 acc_flag = True
 
 # ZeroMQ Context and Sockets
@@ -30,27 +30,34 @@ host_ip= "192.168.66.77"
 if acc_flag:
     acc_socket = context.socket(zmq.REQ)
     acc_socket.connect("tcp://localhost:5555")  
+    acc_socket.setsockopt(zmq.RCVTIMEO, 500)  # Timeout for receiving ACC speed
     poller = zmq.Poller()
     poller.register(acc_socket, zmq.POLLIN)
 
 speed_socket = context.socket(zmq.SUB)
 speed_socket.connect("tcp://"+host_ip+":5556")  
 speed_socket.setsockopt_string(zmq.SUBSCRIBE, '')  
+speed_socket.setsockopt(zmq.RCVTIMEO, 500)  # Timeout for receiving speeds
 
 if __name__ == "__main__":
     try:
         while True:
-            speeds_str = speed_socket.recv()
-            speeds = json.loads(speeds_str.decode("utf-8"))
-            
+            try:
+                speeds_str = speed_socket.recv()
+                speeds = json.loads(speeds_str.decode("utf-8"))
+            except zmq.Again:
+                print("Warning: No speed data received. Skipping this cycle.")
+                continue  # Skip iteration if no data is received
+
             if acc_flag:
-                acc_socket.send(b"GET_SPEED")
-                socks = dict(poller.poll(500))  
-                if acc_socket in socks and socks[acc_socket] == zmq.POLLIN:
+                try:
+                    acc_socket.send(b"GET_SPEED")
                     acc_speed = int(acc_socket.recv().decode())
                     speeds['vx'] = min(speeds['vx'], acc_speed)
-            
-            robot.update_command(speeds['vx'], speeds['vy'], speeds['w'])
+                except zmq.Again:
+                    print("Warning: No ACC speed received. Using last known speed.")
+
+            robot.update_command(speeds.get('vx', 0), speeds.get('vy', 0), speeds.get('w', 0))
             robot.send_speeds_to_serial()
             sleep(0.3)
     except serial.SerialException as e:
